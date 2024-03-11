dd0f571c828c32f5ac256a80ac200837d40836f1
Dominick <adltech02@gmail.com>
2024-03-10 18:58:12 -0600

    Update next.config.mjs with pageExtensions and dir

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    pageExtensions: ['tsx', 'ts'],
+    dir: './app',
+  };
+  
+  export default nextConfig;
+  
